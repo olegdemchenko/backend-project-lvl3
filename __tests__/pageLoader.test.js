e0646112adc81ac744486535add6ffa57cfc0af4
odemch <odemch@softserveinc.com>
2021-12-27 12:39:01 +0200

    fix tests bugs

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -3,14 +3,15 @@ import path from 'path';
 import os from 'os';
 import nock from 'nock';
 
-import downloadPage from '../src';
+import downloadPage from '../src/downloadPage';
+import { rejects } from 'assert';
 
 let template;
 let tempDirPath;
 
 const links = {
-  wiki: "https://en.wikipedia.org/wiki",
-  empty: "https://some-unexisting-page/some-path",
+  wiki: "https://en.wikipedia.org",
+  empty: "https://some-unexisting-page",
 }
 
 beforeAll(async () => {
@@ -22,11 +23,15 @@ beforeEach(async () => {
   tempDirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
+afterAll(() => {
+  nock.enableNetConnect();
+})
+
 test('testing main functionality', async () => {
   nock(links.wiki)
-    .get('/')
+    .get('/wiki')
     .reply(200, template);
-  const pagePath = await downloadPage(tempDirPath, links.wiki);
+  const pagePath = await downloadPage(tempDirPath, links.wiki + '/wiki');
   const downloadedPage = await fs.readFile(pagePath, 'utf-8');
   expect(downloadedPage).toEqual(template);
 });
@@ -35,7 +40,6 @@ test('test error handling', async () => {
   nock(links.empty)
     .get('/')
     .reply(404, 'Not found');
-  expect(async () => {
-    await downloadPage(tempDirPath, links.empty);
-  }).toThrow();
+  await expect(downloadPage(tempDirPath, links.empty)).rejects.toThrow();
 });
+
